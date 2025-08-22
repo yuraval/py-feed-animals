ba2e1e390a780b46420e12f17d0b0dc04ac637ee
Yura Valko <yuravalko166@gmail.com>
2025-08-22 22:44:02 +0300

    Solution6

--- app/main.py
+++ app/main.py
@@ -1,10 +1,13 @@
 class Animal:
-    def __init__(self, name: str, appetite: int, is_hungry: bool = True) -> None:
+    def __init__(self, name: str, appetite: int,
+                 is_hungry: bool = True) -> None:
         self.name = name
         self.appetite = appetite
         self.is_hungry = is_hungry
+
     def print_name(self) -> None:
         print(f"Hello, I'm {self.name}")
+
     def feed(self) -> int:
         if self.is_hungry:
             print(f"Eating {self.appetite} food points...")
@@ -12,16 +15,20 @@ class Animal:
             return self.appetite
         return 0
 
+
 class Cat(Animal):
     def __init__(self, name: str, is_hungry: bool = True) -> None:
         super().__init__(name, 3, is_hungry)
+
     @staticmethod
     def catch_mouse() -> None:
         print("The hunt began!")
 
+
 class Dog(Animal):
     def __init__(self, name: str, is_hungry: bool = True) -> None:
         super().__init__(name, 7, is_hungry)
+
     @staticmethod
     def bring_slippers() -> None:
         print("The slippers delivered!")
