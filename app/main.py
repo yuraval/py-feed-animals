7f625fb00bb8ea71837bfee25d6dcaef5814f8a6
Yura Valko <yuravalko166@gmail.com>
2025-08-22 22:44:54 +0300

    Solution6.1

--- app/main.py
+++ app/main.py
@@ -33,5 +33,6 @@ class Dog(Animal):
     def bring_slippers() -> None:
         print("The slippers delivered!")
 
+
 def feed_animals(animals: list[Animal]) -> int:
     return sum(animal.feed() for animal in animals)
